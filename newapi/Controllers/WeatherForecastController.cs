f48952d562855a00f92fc21b18480821e45a43fc
suran43 <suran43@gmail.com>
2026-02-08 11:32:41 +0530

    com2

--- newapi/Controllers/WeatherForecastController.cs
+++ newapi/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace newapi.Controllers
             })
             .ToArray();
         }
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Ge1t()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
     }
 }
